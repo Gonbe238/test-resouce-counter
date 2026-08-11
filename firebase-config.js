674c5d9a9001140c55999dc037dd1a9ebb4c3da7
Gonbe238 <141091883+Gonbe238@users.noreply.github.com>
2026-08-11 22:57:03 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyCDdmGM9W5XEDL-kZiVP_cJ8wniPzfFwig",
+  authDomain: "test-rsc-cnt.firebaseapp.com",
+  databaseURL: "https://test-rsc-cnt-default-rtdb.firebaseio.com",
+  projectId: "test-rsc-cnt",
+  storageBucket: "test-rsc-cnt.firebasestorage.app",
+  messagingSenderId: "48737023040",
+  appId: "1:48737023040:web:cb51e343b84225dd33dba5",
+  measurementId: "G-Y1RJRLMWCQ"
 };
 
 const app = initializeApp(firebaseConfig);
